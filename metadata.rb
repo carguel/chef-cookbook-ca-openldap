735c8034201d4e4d769cdf189a3259b5d09e42ab
U-CORP\carguel <christophe.arguel@free.fr>
2013-05-08 00:21:17 +0200

    Update metadata for distribution.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'ca_openldap'
 maintainer       'Christophe Arguel'
 maintainer_email 'christophe.arguel@free.fr'
-license          'All rights reserved'
-description      'Installs/Configures Openldap (>=2.4)'
+license          'Apache 2.0'
+description      'Configures a node to be an OpenLDAP server or client.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
