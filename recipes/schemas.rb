2bf6b86f99b3eee69a8f2cde842656a1f0b27961
Christophe Sourisse <christophe.sourisse@capgemini.com>
2017-03-08 14:11:48 +0100

    Use of the "bracket" syntax for node attributes setting/ accessing in the 'schemas' recipe. See commit 9c61fa84889a0482877eae2c70f402013b7daede. See https://docs.chef.io/deprecations_attributes.html.

--- recipes/schemas.rb
+++ recipes/schemas.rb
@@ -20,12 +20,12 @@
 include_recipe 'ca_openldap::default'
 include_recipe 'ca_openldap::server'
 
-schema_dir = node.ca_openldap.schema_dir
+schema_dir = node['ca_openldap']['schema_dir']
 ldif_dir = "/tmp/ldif_schemas"
 
 # Load all configured core LDAP schemas to LDAP database
 # (N.B. by default, only the core schema is loaded)
-node.ca_openldap.default_schemas.each do |schema_name|
+node['ca_openldap']['default_schemas'].each do |schema_name|
   load_ldap_schema do
     schema_name schema_name
     schema_path "#{schema_dir}/#{schema_name}.ldif"
@@ -34,24 +34,24 @@ end
 
 # Copy the additional schemas from the cookbook file distribution
 remote_directory schema_dir do
-  cookbook node.ca_openldap.schema_cookbook
+  cookbook node['ca_openldap']['schema_cookbook']
   source "schemas"
   action :create
   files_mode 00644
   files_owner 'root'
   files_group 'root'
-  not_if { node.ca_openldap.additional_schemas.empty? }
+  not_if { node['ca_openldap']['additional_schemas'].empty? }
 end
 
 #convert additional schemas as LDIF
 ldif_additional_schemas do
   ldif_dir ldif_dir
   schema_dir schema_dir
-  not_if { node.ca_openldap.additional_schemas.empty? }
+  not_if { node['ca_openldap']['additional_schemas'].empty? }
 end
 
 #import additional schemas into LDAP
-node.ca_openldap.additional_schemas.each do |schema_name|
+node['ca_openldap']['additional_schemas'].each do |schema_name|
   ldap_additional_schema "ldap_schema_#{schema_name}" do
     ldif_dir ldif_dir
     schema schema_name
